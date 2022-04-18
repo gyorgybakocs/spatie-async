e7e8880e94b907d020b144c2557900ee9a389fb8
gyorgybakocs <gyorgy.bakocs@transprnt.com>
2022-04-18 18:10:32 +0200

    ok

--- src/Task.php
+++ src/Task.php
@@ -4,6 +4,12 @@ namespace Spatie\Async;
 
 abstract class Task
 {
+    public function __construct(...$args) {
+        foreach ($args as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+
     abstract public function configure();
 
     abstract public function run();
